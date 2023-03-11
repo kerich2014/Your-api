e84237d0220197136c30bbef8ffd16d57b2d5cb6
Kerich2014 <252222223a@gmail.com>
2023-03-11 19:01:00 +0300

    Done api for returning your IP adress

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 const button = document.querySelector('#btn')
-button.addEventListener('click', () => {
-    const img = document.querySelector('#img')
-let date = document.querySelector('input').value
+let select = document.querySelector('#select')
+const img = document.querySelector('#img')
+const dateInput = document.querySelector('input')
+const date = document.querySelector('input').value
+const outputBlock = document.querySelector('#apimage')
 
-fetch(`https://api.nasa.gov/planetary/apod?date=${date}&hd=true&api_key=iG4f8GwqFUUPUfFdFY0ZVaRX3S1UTjzUdvN4dTc7`)
-.then(res => res.json())
-.then(response => {
-    console.log(response)
-    img.src = response.hdurl
-})
+    button.addEventListener('click', () => {
+    let date = document.querySelector('input').value
+    if(select.value == 'NASA'){
+    fetch(`https://api.nasa.gov/planetary/apod?date=${date}&hd=true&api_key=iG4f8GwqFUUPUfFdFY0ZVaRX3S1UTjzUdvN4dTc7`)
+    .then(res => res.json())
+    .then(response => {
+        console.log(response)
+        img.src = response.hdurl
+    })
+    }
+
+    if(select.value == 'IP'){
+        img.remove()
+        dateInput.remove()
+        const ipText = document.createElement('h1')
+        ipText.classList.add('h1')
+        outputBlock.append(ipText)
+        fetch('https://api.ipify.org?format=json')
+        .then(res => res.json())
+        .then(response => {
+            console.log(response)
+            ipText.innerHTML = `Your IP is: ${response.ip}`
+        })
+    }
 })
 
